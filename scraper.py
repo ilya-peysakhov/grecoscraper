3052c2e3ba4d0d5b7ac2ac91dc9252203e905574
ilya-peysakhov <44712179+ilya-peysakhov@users.noreply.github.com>
2024-03-10 20:53:06 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -29,6 +29,7 @@ config = yaml.safe_load(open('scrape_ufc_stats_config.yaml'))
 events_url = config['completed_events_all_url']
 soup = LIB.get_soup(events_url)
 
+@st.cache_data
 def getEvents():
     all_event_details_df = LIB.parse_event_details(soup)
     return all_event_details_df
@@ -47,6 +48,7 @@ all_fight_details_df = pd.DataFrame(columns=config['fight_details_column_names']
 #     all_fight_details_df = pd.concat([all_fight_details_df, fight_details_df])
 
 # Get all fight details in a list comprehension
+@st.cache_data
 def getFD():
     all_fight_details = [LIB.parse_fight_details(LIB.get_soup(url)) for url in list_of_events_urls]
 # Concatenate all fight details dataframes at once
@@ -57,39 +59,32 @@ with st.popover("View Details DF"):
     all_fight_details_df = getFD()
     st.dataframe(all_fight_details_df,hide_index=True)
 
-# # write fight details to file
-# all_fight_details_df.to_csv(config['fight_details_file_name'], index=False)
-# list_of_fight_details_urls = list(all_fight_details_df['URL'])
-# all_fight_results_df = pd.DataFrame(columns=config['fight_results_column_names'])
-# # create empty df to store fight stats
-# all_fight_stats_df = pd.DataFrame(columns=config['fight_stats_column_names'])
+list_of_fight_details_urls = list(all_fight_details_df['URL'])
+all_fight_results_df = pd.DataFrame(columns=config['fight_results_column_names'])
+all_fight_stats_df = pd.DataFrame(columns=config['fight_stats_column_names'])
 
-# # loop through each fight and parse fight results and stats
-# for url in tqdm_notebook(list_of_fight_details_urls):
+@st.cache_data
+def getResultsStats():
+    for url in list_of_fight_details_urls:
+        # get soup
+        soup = LIB.get_soup(url)
+        # parse fight results and fight stats
+        fight_results_df, fight_stats_df = LIB.parse_organise_fight_results_and_stats(
+            soup,
+            url,
+            config['fight_results_column_names'],
+            config['totals_column_names'],
+            config['significant_strikes_column_names']
+            )
+        # concat fight results
+        all_fight_results_df = pd.concat([all_fight_results_df, fight_results_df])
+        # concat fight stats
+        all_fight_stats_df = pd.concat([all_fight_stats_df, fight_stats_df])
+    return all_fight_results_df,all_fight_stats_df
 
-#     # get soup
-#     soup = LIB.get_soup(url)
-
-#     # parse fight results and fight stats
-#     fight_results_df, fight_stats_df = LIB.parse_organise_fight_results_and_stats(
-#         soup,
-#         url,
-#         config['fight_results_column_names'],
-#         config['totals_column_names'],
-#         config['significant_strikes_column_names']
-#         )
-
-#     # concat fight results
-#     all_fight_results_df = pd.concat([all_fight_results_df, fight_results_df])
-#     # concat fight stats
-#     all_fight_stats_df = pd.concat([all_fight_stats_df, fight_stats_df])
-
-# # show all fight results
-# st.write(all_fight_results_df.head())
-# # show all fight stats
-# st.write(all_fight_stats_df.head())
-
-# write to file
-# all_fight_results_df.to_csv(config['fight_results_file_name'], index=False)
-# write to file
-# all_fight_stats_df.to_csv(config['fight_stats_file_name'], index=False)
+# show all fight results
+with st.popover('View Results DF'):
+    st.dataframe(all_fight_results_df,hide_index=True)
+# show all fight stats
+with st.popover('View Stats DF'):
+    st.dataframe(all_fight_stats_df,hide_index=True))
